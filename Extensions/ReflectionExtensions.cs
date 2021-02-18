f24068b3d6c3003367dcb1c7e7485bea16727a23
Vladyslav Zaiets <rutova2@gmail.com>
2021-02-18 11:18:12 +0200

    small refactor - ConflictResolutionEngine

---
+++ Extensions/ReflectionExtensions.cs
@@ -0,0 +1,152 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Reflection;
+
+namespace EfMigrationDiff.Extensions;
+
+/// <summary>
+/// Extension methods for reflection operations.
+/// Provides utilities for type inspection, property access, and dynamic invocation.
+/// </summary>
+public static class ReflectionExtensions
+{
+    /// <summary>
+    /// Gets all public properties from a type.
+    /// </summary>
+    public static IEnumerable<PropertyInfo> GetPublicProperties(this Type type)
+    {
+        return type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+    }
+
+    /// <summary>
+    /// Gets all public methods from a type.
+    /// </summary>
+    public static IEnumerable<MethodInfo> GetPublicMethods(this Type type)
+    {
+        return type.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+    }
+
+    /// <summary>
+    /// Checks if a type implements a specific interface.
+    /// </summary>
+    public static bool ImplementsInterface<TInterface>(this Type type) where TInterface : class
+    {
+        return typeof(TInterface).IsAssignableFrom(type);
+    }
+
+    /// <summary>
+    /// Checks if a type is a simple/scalar type (not a complex object).
+    /// </summary>
+    public static bool IsSimpleType(this Type type)
+    {
+        return type.IsPrimitive ||
+               type == typeof(string) ||
+               type == typeof(decimal) ||
+               type == typeof(DateTime) ||
+               type == typeof(TimeSpan) ||
+               type == typeof(Guid) ||
+               (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>) &&
+                IsSimpleType(type.GetGenericArguments()[0]));
+    }
+
+    /// <summary>
+    /// Gets the value of a property from an object, returns null if property doesn't exist.
+    /// </summary>
+    public static object? GetPropertyValue(this object? obj, string propertyName)
+    {
+        if (obj == null || string.IsNullOrEmpty(propertyName))
+            return null;
+
+        var property = obj.GetType().GetProperty(propertyName, BindingFlags.Public | BindingFlags.IgnoreCase | BindingFlags.Instance);
+        return property?.GetValue(obj);
+    }
+
+    /// <summary>
+    /// Sets the value of a property on an object.
+    /// </summary>
+    public static bool SetPropertyValue(this object? obj, string propertyName, object? value)
+    {
+        if (obj == null || string.IsNullOrEmpty(propertyName))
+            return false;
+
+        var property = obj.GetType().GetProperty(propertyName, BindingFlags.Public | BindingFlags.IgnoreCase | BindingFlags.Instance);
+        if (property?.CanWrite ?? false)
+        {
+            property.SetValue(obj, value);
+            return true;
+        }
+
+        return false;
+    }
+
+    /// <summary>
+    /// Gets all property values from an object as a dictionary.
+    /// </summary>
+    public static Dictionary<string, object?> GetPropertyDictionary(this object? obj, bool includeNulls = false)
+    {
+        var dict = new Dictionary<string, object?>();
+
+        if (obj == null)
+            return dict;
+
+        var properties = obj.GetType().GetPublicProperties();
+        foreach (var prop in properties)
+        {
+            var value = prop.GetValue(obj);
+            if (includeNulls || value != null)
+            {
+                dict[prop.Name] = value;
+            }
+        }
+
+        return dict;
+    }
+
+    /// <summary>
+    /// Checks if a type has a parameterless constructor.
+    /// </summary>
+    public static bool HasParameterlessConstructor(this Type type)
+    {
+        return type.GetConstructor(Type.EmptyTypes) != null;
+    }
+
+    /// <summary>
+    /// Creates an instance of a type using its parameterless constructor.
+    /// </summary>
+    public static object? CreateInstance(this Type type)
+    {
+        if (!type.HasParameterlessConstructor())
+            return null;
+
+        return Activator.CreateInstance(type);
+    }
+
+    /// <summary>
+    /// Gets all types that implement or inherit from a base type.
+    /// </summary>
+    public static IEnumerable<Type> GetImplementations(this Type baseType, Assembly? assembly = null)
+    {
+        var targetAssembly = assembly ?? Assembly.GetAssembly(baseType);
+        if (targetAssembly == null)
+            return Enumerable.Empty<Type>();
+
+        return targetAssembly.GetTypes()
+            .Where(t => !t.IsAbstract && !t.IsInterface && baseType.IsAssignableFrom(t));
+    }
+
+    /// <summary>
+    /// Gets the friendly name of a type (handles generics).
+    /// </summary>
+    public static string GetFriendlyName(this Type type)
+    {
+        if (!type.IsGenericType)
+            return type.Name;
+
+        var name = type.Name.Substring(0, type.Name.LastIndexOf('`'));
+        var args = type.GetGenericArguments().Select(t => t.GetFriendlyName());
+        return $"{name}<{string.Join(", ", args)}>";
+    }
+}
---
+++ Formatters/JsonFormatter.cs
@@ -0,0 +1,128 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EfMigrationDiff.Formatters;
+
+/// <summary>
+/// JSON formatter for serializing objects to JSON format.
+/// Provides configuration for pretty-printing, null handling, and indentation.
+/// </summary>
+public class JsonFormatter : IOutputFormatter
+{
+    private readonly JsonSerializerOptions _options;
+
+    public JsonFormatter(bool prettyPrint = true, bool includeNulls = false)
+    {
+        _options = new JsonSerializerOptions
+        {
+            WriteIndented = prettyPrint,
+            DefaultIgnoreCondition = includeNulls ? JsonIgnoreCondition.Never : JsonIgnoreCondition.WhenWritingNull,
+            PropertyNameCaseInsensitive = false,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            Converters = new List<JsonConverter>
+            {
+                new JsonStringEnumConverter(JsonNamingPolicy.CamelCase)
+            }
+        };
+    }
+
+    /// <summary>
+    /// Formats an object as JSON string.
+    /// </summary>
+    public string Format(object? obj)
+    {
+        try
+        {
+            return JsonSerializer.Serialize(obj, _options);
+        }
+        catch (Exception ex)
+        {
+            throw new FormattingException($"Failed to serialize to JSON: {ex.Message}", ex);
+        }
+    }
+
+    /// <summary>
+    /// Deserializes JSON string to an object of specified type.
+    /// </summary>
+    public T? Deserialize<T>(string json)
+    {
+        try
+        {
+            return JsonSerializer.Deserialize<T>(json, _options);
+        }
+        catch (Exception ex)
+        {
+            throw new FormattingException($"Failed to deserialize from JSON: {ex.Message}", ex);
+        }
+    }
+
+    /// <summary>
+    /// Deserializes JSON string to object.
+    /// </summary>
+    public object? Deserialize(string json, Type type)
+    {
+        try
+        {
+            return JsonSerializer.Deserialize(json, type, _options);
+        }
+        catch (Exception ex)
+        {
+            throw new FormattingException($"Failed to deserialize from JSON: {ex.Message}", ex);
+        }
+    }
+
+    /// <summary>
+    /// Writes JSON to a file.
+    /// </summary>
+    public void WriteToFile(string filePath, object? obj)
+    {
+        try
+        {
+            var json = Format(obj);
+            File.WriteAllText(filePath, json);
+        }
+        catch (Exception ex)
+        {
+            throw new FormattingException($"Failed to write JSON to file: {ex.Message}", ex);
+        }
+    }
+
+    /// <summary>
+    /// Reads JSON from a file.
+    /// </summary>
+    public T? ReadFromFile<T>(string filePath)
+    {
+        try
+        {
+            var json = File.ReadAllText(filePath);
+            return Deserialize<T>(json);
+        }
+        catch (Exception ex)
+        {
+            throw new FormattingException($"Failed to read JSON from file: {ex.Message}", ex);
+        }
+    }
+}
+
+/// <summary>
+/// Interface for output formatters.
+/// </summary>
+public interface IOutputFormatter
+{
+    string Format(object? obj);
+    void WriteToFile(string filePath, object? obj);
+}
+
+/// <summary>
+/// Exception for formatting operations.
+/// </summary>
+public class FormattingException : Exception
+{
+    public FormattingException(string message) : base(message) { }
+    public FormattingException(string message, Exception innerException) : base(message, innerException) { }
+}
