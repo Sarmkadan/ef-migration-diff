d80bd4b89108123343bb9b62183d86621a30763e
Vladyslav Zaiets <rutova2@gmail.com>
2021-05-03 15:44:27 +0300

    refactor: tweak MigrationImpactAnalyzer
    found this while looking at something else.

--- Formatters/VisualDiffFormatter.cs
+++ Formatters/VisualDiffFormatter.cs
@@ -1,9 +1,4 @@
 #nullable enable
-// =============================================================================
-// Author: Vladyslav Zaiets | https://sarmkadan.com
-// CTO & Software Architect
-// =============================================================================
-
 using System.Net;
 using System.Text;
 using EfMigrationDiff.Interfaces;
--- tests/ef-migration-diff.Tests/ValidationHelperTests.cs
+++ tests/ef-migration-diff.Tests/ValidationHelperTests.cs
@@ -1,9 +1,4 @@
 #nullable enable
-// =============================================================================
-// Author: Vladyslav Zaiets | https://sarmkadan.com
-// CTO & Software Architect
-// =============================================================================
-
 using EfMigrationDiff.Utilities;
 using FluentAssertions;
 
