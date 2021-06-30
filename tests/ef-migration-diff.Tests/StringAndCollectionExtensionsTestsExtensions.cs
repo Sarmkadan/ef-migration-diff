751caa11c86e8120fef3c99a82f1faf4529e8e6e
Vladyslav Zaiets <rutova2@gmail.com>
2021-06-30 12:48:31 +0300

    feat: extension methods on StringAndCollectionExtensions

--- tests/ef-migration-diff.Tests/StringAndCollectionExtensionsTestsExtensions.cs
+++ tests/ef-migration-diff.Tests/StringAndCollectionExtensionsTestsExtensions.cs
@@ -5,10 +5,21 @@ using System.Reflection;
 
 namespace EfMigrationDiff.Tests
 {
+    /// <summary>
+    /// Provides extension methods to run and manage tests for <see cref="StringAndCollectionExtensionsTests"/>.
+    /// This class contains helper methods for executing test methods in bulk and collecting test metadata.
+    /// </summary>
     public static class StringAndCollectionExtensionsTestsExtensions
     {
+        /// <summary>
+        /// Runs all string conversion tests (ToPascalCase, ToSnakeCase, Truncate) on the test instance.
+        /// </summary>
+        /// <param name="testInstance">The test instance containing the methods to invoke.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="testInstance"/> is null.</exception>
         public static void RunAllStringConversionTests(this StringAndCollectionExtensionsTests testInstance)
         {
+            ArgumentNullException.ThrowIfNull(testInstance);
+
             var stringMethods = typeof(StringAndCollectionExtensionsTests)
                 .GetMethods(BindingFlags.Public | BindingFlags.Instance)
                 .Where(m => m.Name.StartsWith("ToPascalCase") || m.Name.StartsWith("ToSnakeCase") || m.Name.StartsWith("Truncate"))
@@ -20,8 +31,15 @@ namespace EfMigrationDiff.Tests
             }
         }
 
+        /// <summary>
+        /// Runs all collection batching tests (Batch) on the test instance.
+        /// </summary>
+        /// <param name="testInstance">The test instance containing the methods to invoke.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="testInstance"/> is null.</exception>
         public static void RunAllCollectionBatchingTests(this StringAndCollectionExtensionsTests testInstance)
         {
+            ArgumentNullException.ThrowIfNull(testInstance);
+
             var batchMethods = typeof(StringAndCollectionExtensionsTests)
                 .GetMethods(BindingFlags.Public | BindingFlags.Instance)
                 .Where(m => m.Name.StartsWith("Batch"))
@@ -33,16 +51,36 @@ namespace EfMigrationDiff.Tests
             }
         }
 
+        /// <summary>
+        /// Gets the names of all public instance methods in <see cref="StringAndCollectionExtensionsTests"/>.
+        /// </summary>
+        /// <param name="testInstance">The test instance (unused, required for extension method syntax).</param>
+        /// <returns>A list of method names.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="testInstance"/> is null.</exception>
         public static List<string> GetAllTestNames(this StringAndCollectionExtensionsTests testInstance)
         {
+            ArgumentNullException.ThrowIfNull(testInstance);
+
             return typeof(StringAndCollectionExtensionsTests)
                 .GetMethods(BindingFlags.Public | BindingFlags.Instance)
                 .Select(m => m.Name)
                 .ToList();
         }
 
+        /// <summary>
+        /// Invokes all public instance methods on the test instance and returns true if all succeed.
+        /// </summary>
+        /// <param name="testInstance">The test instance containing the methods to invoke.</param>
+        /// <returns>True if all tests pass; otherwise, false.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="testInstance"/> is null.</exception>
+        /// <remarks>
+        /// This method catches exceptions and logs them to the console, allowing execution to continue
+        /// through all methods even if some fail. This is useful for bulk test execution scenarios.
+        /// </remarks>
         public static bool AssertAllTestsPass(this StringAndCollectionExtensionsTests testInstance)
         {
+            ArgumentNullException.ThrowIfNull(testInstance);
+
             var allMethods = typeof(StringAndCollectionExtensionsTests)
                 .GetMethods(BindingFlags.Public | BindingFlags.Instance)
                 .ToList();
@@ -55,10 +93,11 @@ namespace EfMigrationDiff.Tests
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine($"Test failed: {method.Name} - {ex.Message}");
+                    Console.WriteLine($"Test failed: {method.Name} - {ex.InnerException?.Message ?? ex.Message}");
                     return false;
                 }
             }
+
             return true;
         }
     }
