af52e99439eadb3ce9bed20ea4fa02fb18f04323
Vladyslav Zaiets <rutova2@gmail.com>
2021-07-02 06:22:38 +0300

    Serialize StringExtensionsJson properly

--- Extensions/StringExtensionsJsonExtensions.cs
+++ Extensions/StringExtensionsJsonExtensions.cs
@@ -9,6 +9,9 @@ using System.Text.Json.Serialization;
 /// System.Text.Json serialization extensions for string values with camelCase naming policy.
 /// Provides methods to serialize and deserialize strings to/from JSON.
 /// </summary>
+/// <remarks>
+/// This class is sealed to prevent inheritance and enable potential runtime optimizations.
+/// </remarks>
 public static class StringExtensionsJsonExtensions
 {
     private static readonly JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions
@@ -24,13 +27,13 @@ public static class StringExtensionsJsonExtensions
     /// <param name="value">The string value to serialize.</param>
     /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
     /// <returns>A JSON string representation of the string value.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
     public static string ToJson(this string value, bool indented = false)
     {
+        ArgumentNullException.ThrowIfNull(value);
+
         var options = indented
-            ? new JsonSerializerOptions(_jsonSerializerOptions)
-            {
-                WriteIndented = true
-            }
+            ? new JsonSerializerOptions(_jsonSerializerOptions) { WriteIndented = true }
             : _jsonSerializerOptions;
 
         return JsonSerializer.Serialize(value, options);
@@ -40,9 +43,12 @@ public static class StringExtensionsJsonExtensions
     /// Deserializes a JSON string to a string value.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>The deserialized string value, or null if the JSON is invalid.</returns>
+    /// <returns>The deserialized string value, or <see langword="null"/> if the JSON is invalid or whitespace.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
     public static string? FromJson(string json)
     {
+        ArgumentNullException.ThrowIfNull(json);
+
         if (string.IsNullOrWhiteSpace(json))
         {
             return null;
@@ -62,10 +68,13 @@ public static class StringExtensionsJsonExtensions
     /// Attempts to deserialize a JSON string to a string value.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">The deserialized string value, or null if deserialization fails.</param>
-    /// <returns>True if deserialization succeeds; otherwise, false.</returns>
+    /// <param name="value">The deserialized string value, or <see langword="null"/> if deserialization fails.</param>
+    /// <returns><see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/>.</exception>
     public static bool TryFromJson(string json, out string? value)
     {
+        ArgumentNullException.ThrowIfNull(json);
+
         value = null;
 
         if (string.IsNullOrWhiteSpace(json))
