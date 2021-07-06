d76f43bba9700b32b009171f87bd68f3765bd425
Vladyslav Zaiets <rutova2@gmail.com>
2021-07-06 06:01:52 +0300

    Json serialization for ReportEngineJson

--- Reports/ReportEngineJsonExtensions.cs
+++ Reports/ReportEngineJsonExtensions.cs
@@ -3,65 +3,88 @@ using System.Text.Json.Serialization;
 
 namespace EfMigrationDiff.Reports;
 
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for <see cref="ReportEngine"/>.
+/// </summary>
 public static class ReportEngineJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
-    {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-        WriteIndented = false,
-        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-    };
+	private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+	{
+		PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		WriteIndented = false,
+		DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+	};
 
-    public static string ToJson(this ReportEngine value, bool indented = false)
-    {
-        if (value == null)
-        {
-            return "{}";
-        }
+	/// <summary>
+	/// Serializes a <see cref="ReportEngine"/> instance to a JSON string.
+	/// </summary>
+	/// <param name="value">The <see cref="ReportEngine"/> instance to serialize.</param>
+	/// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+	/// <returns>A JSON string representation of the <see cref="ReportEngine"/> instance.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/></exception>
+	public static string ToJson(this ReportEngine value, bool indented = false)
+	{
+		ArgumentNullException.ThrowIfNull(value);
 
-        var options = indented
-            ? new JsonSerializerOptions(_jsonOptions)
-            {
-                WriteIndented = true
-            }
-            : _jsonOptions;
+		var options = indented
+			? new JsonSerializerOptions(_jsonOptions)
+			{
+				WriteIndented = true
+			} : _jsonOptions;
 
-        return JsonSerializer.Serialize(value, options);
-    }
+		return JsonSerializer.Serialize(value, options);
+	}
 
-    public static ReportEngine? FromJson(string json)
-    {
-        if (string.IsNullOrWhiteSpace(json))
-        {
-            return null;
-        }
+	/// <summary>
+	/// Deserializes a JSON string to a <see cref="ReportEngine"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <returns>The deserialized <see cref="ReportEngine"/> instance, or <see langword="null"/> if deserialization fails.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/></exception>
+	public static ReportEngine? FromJson(string json)
+	{
+		ArgumentNullException.ThrowIfNull(json);
 
-        try
-        {
-            return JsonSerializer.Deserialize<ReportEngine>(json, _jsonOptions);
-        }
-        catch (JsonException)
-        {
-            return null;
-        }
-    }
+		if (string.IsNullOrWhiteSpace(json))
+		{
+			return null;
+		}
 
-    public static bool TryFromJson(string json, out ReportEngine? value)
-    {
-        value = null;
-        if (string.IsNullOrWhiteSpace(json))
-        {
-            return false;
-        }
+		try
+		{
+			return JsonSerializer.Deserialize<ReportEngine>(json, _jsonOptions);
+		}
+		catch (JsonException)
+		{
+			return null;
+		}
+	}
 
-        try
-        {
-            value = JsonSerializer.Deserialize<ReportEngine>(json, _jsonOptions);
-            return true;
-        }
-        catch (JsonException)
-        {
-            return false;
-        }
-    }
+	/// <summary>
+	/// Attempts to deserialize a JSON string to a <see cref="ReportEngine"/> instance.
+	/// </summary>
+	/// <param name="json">The JSON string to deserialize.</param>
+	/// <param name="value">Receives the deserialized <see cref="ReportEngine"/> instance if successful; otherwise, <see langword="null"/>.</param>
+	/// <returns><see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.</returns>
+	/// <exception cref="ArgumentNullException"><paramref name="json"/> is <see langword="null"/></exception>
+	public static bool TryFromJson(string json, out ReportEngine? value)
+	{
+		ArgumentNullException.ThrowIfNull(json);
+
+		value = null;
+		if (string.IsNullOrWhiteSpace(json))
+		{
+			return false;
+		}
+
+		try
+		{
+			value = JsonSerializer.Deserialize<ReportEngine>(json, _jsonOptions);
+			return true;
+		}
+		catch (JsonException)
+		{
+			return false;
+		}
+	}
 }
