a4a18357bb2540fac78932d9ebc57cad225f65a1
Vladyslav Zaiets <rutova2@gmail.com>
2021-07-12 14:48:12 +0300

    a couple of MigrationDiff extensions

--- Services/MigrationDiffServiceExtensions.cs
+++ Services/MigrationDiffServiceExtensions.cs
@@ -2,13 +2,20 @@
 
 using EfMigrationDiff.Models;
 using Microsoft.Extensions.Logging;
+using System.Linq;
 
 namespace EfMigrationDiff.Services;
 
 /// <summary>
-/// Extension methods for <see cref="MigrationDiffService"/> providing additional functionality
-/// for migration comparison and analysis.
+/// Provides extension methods for <see cref="MigrationDiffService"/> that offer additional
+/// functionality for analyzing and reporting on migration differences between branches.
 /// </summary>
+/// <remarks>
+/// This static class contains methods for generating reports, checking for destructive changes,
+/// identifying common migrations, and determining merge safety. All public methods include
+/// proper null checking via <see cref="ArgumentNullException.ThrowIfNull"/> and follow
+/// C# idioms for extension methods.
+/// </remarks>
 public static class MigrationDiffServiceExtensions
 {
     /// <summary>
@@ -18,7 +25,8 @@ public static class MigrationDiffServiceExtensions
     /// </summary>
     /// <param name="service">The migration diff service instance.</param>
     /// <param name="diff">The migration diff result to summarize.</param>
-    /// <returns>A concise multi-line string with key comparison metrics.</returns>
+    /// <returns>A multi-line string containing migration counts, schema changes, conflicts, and a recommendation.</returns>
+    /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="service"/> or <paramref name="diff"/> is null.</exception>
     public static string GenerateQuickReport(this MigrationDiffService service, MigrationDiff diff)
     {
         ArgumentNullException.ThrowIfNull(service);
@@ -32,19 +40,19 @@ public static class MigrationDiffServiceExtensions
         report.AppendLine();
 
         report.AppendLine("Migration Counts:");
-        report.AppendLine($"  Source Only: {diff.OnlyInSource.Count}");
-        report.AppendLine($"  Target Only: {diff.OnlyInTarget.Count}");
-        report.AppendLine($"  Common: {diff.InBoth.Count}");
+        report.AppendLine($" Source Only: {diff.OnlyInSource.Count}");
+        report.AppendLine($" Target Only: {diff.OnlyInTarget.Count}");
+        report.AppendLine($" Common: {diff.InBoth.Count}");
         report.AppendLine();
 
         report.AppendLine("Schema Changes:");
-        report.AppendLine($"  Total: {diff.GetTotalSchemaChanges()}");
-        report.AppendLine($"  Destructive: {diff.GetDestructiveChanges().Count}");
+        report.AppendLine($" Total: {diff.GetTotalSchemaChanges()}");
+        report.AppendLine($" Destructive: {diff.GetDestructiveChanges().Count}");
         report.AppendLine();
 
         report.AppendLine("Conflicts:");
-        report.AppendLine($"  Total: {diff.Conflicts.Count}");
-        report.AppendLine($"  Blocking: {diff.GetBlockingConflicts()}");
+        report.AppendLine($" Total: {diff.Conflicts.Count}");
+        report.AppendLine($" Blocking: {diff.GetBlockingConflicts()}");
         report.AppendLine();
 
         report.AppendLine("Recommendation:");
@@ -61,6 +69,7 @@ public static class MigrationDiffServiceExtensions
     /// <param name="service">The migration diff service instance.</param>
     /// <param name="diff">The migration diff result to check.</param>
     /// <returns>True if destructive changes exist, otherwise false.</returns>
+    /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="service"/> or <paramref name="diff"/> is null.</exception>
     public static bool HasDestructiveChanges(this MigrationDiffService service, MigrationDiff diff)
     {
         ArgumentNullException.ThrowIfNull(service);
@@ -76,6 +85,7 @@ public static class MigrationDiffServiceExtensions
     /// <param name="service">The migration diff service instance.</param>
     /// <param name="diff">The migration diff result to analyze.</param>
     /// <returns>A list of migration names present in both branches.</returns>
+    /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="service"/> or <paramref name="diff"/> is null.</exception>
     public static List<string> GetCommonMigrationNames(this MigrationDiffService service, MigrationDiff diff)
     {
         ArgumentNullException.ThrowIfNull(service);
@@ -91,6 +101,7 @@ public static class MigrationDiffServiceExtensions
     /// <param name="service">The migration diff service instance.</param>
     /// <param name="diff">The migration diff result to analyze.</param>
     /// <returns>A formatted string containing conflict details.</returns>
+    /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="service"/> or <paramref name="diff"/> is null.</exception>
     public static string GenerateConflictReport(this MigrationDiffService service, MigrationDiff diff)
     {
         ArgumentNullException.ThrowIfNull(service);
@@ -112,9 +123,9 @@ public static class MigrationDiffServiceExtensions
         foreach (var conflict in diff.Conflicts.OrderByDescending(c => c.Severity))
         {
             report.AppendLine($"- {conflict.GetTitle()}: {conflict.Description}");
-            report.AppendLine($"  Severity: {conflict.Severity}");
-            report.AppendLine($"  Blocking: {conflict.IsBlocking()}");
-            report.AppendLine($"  Migrations: {conflict.FirstMigrationId} ↔ {conflict.SecondMigrationId}");
+            report.AppendLine($" Severity: {conflict.Severity}");
+            report.AppendLine($" Blocking: {conflict.IsBlocking()}");
+            report.AppendLine($" Migrations: {conflict.FirstMigrationId} ↔ {conflict.SecondMigrationId}");
             report.AppendLine();
         }
 
@@ -130,6 +141,7 @@ public static class MigrationDiffServiceExtensions
     /// <param name="service">The migration diff service instance.</param>
     /// <param name="diff">The migration diff result to evaluate.</param>
     /// <returns>True if safe to merge, false if conflicts or destructive changes exist.</returns>
+    /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="service"/> or <paramref name="diff"/> is null.</exception>
     public static bool CanMergeSafely(this MigrationDiffService service, MigrationDiff diff)
     {
         ArgumentNullException.ThrowIfNull(service);
@@ -143,34 +155,20 @@ public static class MigrationDiffServiceExtensions
     /// </summary>
     /// <param name="diff">The migration diff result.</param>
     /// <returns>A recommendation string.</returns>
+    /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="diff"/> is null.</exception>
     private static string GetRecommendation(MigrationDiff diff)
     {
-        if (diff.Result == ComparisonResult.Identical)
-        {
-            return "✓ Migrations are identical - safe to merge";
-        }
+        ArgumentNullException.ThrowIfNull(diff);
 
-        if (diff.Result == ComparisonResult.Similar)
+        return diff.Result switch
         {
-            return "✓ Minor differences detected - review schema changes before merging";
-        }
-
-        if (diff.Result == ComparisonResult.Different && !diff.HasConflicts())
-        {
-            return "⚠ Significant differences detected - manual review recommended";
-        }
-
-        if (diff.HasBlockingConflicts())
-        {
-            return "✗ BLOCKING CONFLICTS DETECTED - DO NOT MERGE";
-        }
-
-        if (diff.HasDestructiveChanges())
-        {
-            return "✗ DESTRUCTIVE CHANGES DETECTED - manual data backup required before merge";
-        }
-
-        return "⚠ Review required - conflicts or significant differences detected";
+            ComparisonResult.Identical => "✓ Migrations are identical - safe to merge",
+            ComparisonResult.Similar => "✓ Minor differences detected - review schema changes before merging",
+            ComparisonResult.Different when !diff.HasConflicts() => "⚠ Significant differences detected - manual review recommended",
+            _ when diff.HasBlockingConflicts() => "✗ BLOCKING CONFLICTS DETECTED - DO NOT MERGE",
+            _ when diff.HasDestructiveChanges() => "✗ DESTRUCTIVE CHANGES DETECTED - manual data backup required before merge",
+            _ => "⚠ Review required - conflicts or significant differences detected"
+        };
     }
 
     /// <summary>
@@ -178,26 +176,20 @@ public static class MigrationDiffServiceExtensions
     /// </summary>
     /// <param name="diff">The migration diff result.</param>
     /// <returns>A recommendation string for merge decisions.</returns>
+    /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="diff"/> is null.</exception>
     private static string GetMergeRecommendation(MigrationDiff diff)
     {
+        ArgumentNullException.ThrowIfNull(diff);
+
         var blockingCount = diff.GetBlockingConflicts();
         var destructiveCount = diff.GetDestructiveChanges().Count;
 
-        if (blockingCount > 0)
+        return (blockingCount, destructiveCount, diff.Conflicts.Count) switch
         {
-            return "BLOCK merge until conflicts are resolved. Critical conflicts detected.";
-        }
-
-        if (destructiveCount > 0)
-        {
-            return "REVIEW merge carefully. Destructive changes require data backup and manual intervention.";
-        }
-
-        if (diff.Conflicts.Count > 3)
-        {
-            return "Multiple conflicts detected. Manual merge and extensive testing required.";
-        }
-
-        return "Conflicts present but may be resolvable. Manual review recommended before merging.";
+            ( > 0, _, _) => "BLOCK merge until conflicts are resolved. Critical conflicts detected.",
+            (_, > 0, _) => "REVIEW merge carefully. Destructive changes require data backup and manual intervention.",
+            (_, _, > 3) => "Multiple conflicts detected. Manual merge and extensive testing required.",
+            _ => "Conflicts present but may be resolvable. Manual review recommended before merging."
+        };
     }
 }
