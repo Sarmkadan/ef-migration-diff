8c0c74737a44801514432b47e90e884c2f10b1cf
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 17:23:35 +0300

    refactor: tidy up ReportGenerationServiceJsonExtensions

--- Services/ReportGenerationServiceJsonExtensions.cs
+++ Services/ReportGenerationServiceJsonExtensions.cs
@@ -24,16 +24,13 @@ public static class ReportGenerationServiceJsonExtensions
     /// <param name="value">The service instance to serialize.</param>
     /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
     /// <returns>A JSON string representation of the service.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
     public static string ToJson(this ReportGenerationService value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
 
         var options = indented
-            ? new JsonSerializerOptions(_jsonOptions)
-            {
-                WriteIndented = true
-            }
+            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
             : _jsonOptions;
 
         return JsonSerializer.Serialize(value, options);
@@ -43,7 +40,7 @@ public static class ReportGenerationServiceJsonExtensions
     /// Deserializes a JSON string to a <see cref="ReportGenerationService"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>The deserialized service instance, or null if the JSON is empty or whitespace.</returns>
+    /// <returns>The deserialized service instance, or <see langword="null"/> if the JSON is empty or whitespace.</returns>
     /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
     public static ReportGenerationService? FromJson(string json)
     {
@@ -60,7 +57,7 @@ public static class ReportGenerationServiceJsonExtensions
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
     /// <param name="value">Receives the deserialized service instance if successful.</param>
-    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
     public static bool TryFromJson(string json, out ReportGenerationService? value)
     {
         value = null;
