c88a9dff1415bfcc104cadfcecba6f509ba75aa5
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 16:01:06 +0300

    refactor: cleanup in RequestLoggingMiddlewareValidation

--- Middleware/RequestLoggingMiddlewareValidation.cs
+++ Middleware/RequestLoggingMiddlewareValidation.cs
@@ -2,7 +2,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Globalization;
 
 namespace EfMigrationDiff.Middleware;
 
@@ -22,16 +21,7 @@ public static class RequestLoggingMiddlewareValidation
     {
         ArgumentNullException.ThrowIfNull(value);
 
-        var problems = new List<string>();
-
-        // Validate the logger field (cannot be null due to constructor validation)
-        // The logger is validated during construction, so we just check it's not null
-        if (value.GetType().GetField("_logger", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.GetValue(value) is null)
-        {
-            problems.Add("Logger instance is null.");
-        }
-
-        return problems.AsReadOnly();
+        return Array.Empty<string>();
     }
 
     /// <summary>
@@ -39,10 +29,7 @@ public static class RequestLoggingMiddlewareValidation
     /// </summary>
     /// <param name="value">The middleware instance to check.</param>
     /// <returns><see langword="true"/> if valid; otherwise, <see langword="false"/>.</returns>
-    public static bool IsValid(this RequestLoggingMiddleware value)
-    {
-        return value.Validate().Count == 0;
-    }
+    public static bool IsValid(this RequestLoggingMiddleware value) => value.Validate().Count == 0;
 
     /// <summary>
     /// Ensures that the specified <see cref="RequestLoggingMiddleware"/> instance is valid.
