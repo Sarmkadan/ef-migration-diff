428b7c1a331ddd531e036eb1a10f5450b9391d61
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-17 23:41:57 +0000

    chore: maintenance updates

--- src/Services/SchemaChangeDetectorService.cs
+++ src/Services/SchemaChangeDetectorService.cs
@@ -8,6 +8,13 @@ namespace Services
 {
     public class SchemaChangeDetectorService
     {
+        /// <summary>
+        /// Detects schema changes for the specified database object.
+        /// </summary>
+        /// <param name="schemaName">The name of the schema containing the object.</param>
+        /// <param name="tableName">The name of the table to inspect for changes.</param>
+        /// <param name="columnName">The name of the column to inspect for changes.</param>
+        /// <param name="cancellationToken">A token that can be used to cancel the operation.</param>
         public async Task DetectChangesAsync(string schemaName, string tableName, string columnName, CancellationToken cancellationToken = default)
         {
             try
